532a292b2240652abe16a9ee9b5ab88576f7a0bc
Ludovico Mariani <mariani.ludovico.studente@itispaleocapa.it>
2023-01-13 12:29:53 +0100

    inizio

--- Mariani_21/Program.cs
+++ Mariani_21/Program.cs
@@ -11,5 +11,30 @@ namespace Mariani_21
         static void Main(string[] args)
         {
         }
+
+        static int InsertArray()
+        {
+            return 0;
+        }
+
+        static string StringaHTML()
+        {
+            return "0";
+        }
+
+        static int TrovaArray()
+        {
+            return 0;
+        }
+
+        static int CancArray()
+        {
+            return 0;
+        }
+
+        static int InserimentoPosArray()
+        {
+            return 0;
+        }
     }
 }
